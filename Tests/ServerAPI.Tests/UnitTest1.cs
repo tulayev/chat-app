54ccf66ac19f9ac10a25587541ab6913ceb4ce36
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2025-09-25 22:09:03 +0500

    first commit

---
+++ Tests/ServerAPI.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ServerAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WPFClient/WebSocketClient.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace WPFClient
+{
+    internal class WebSocketClient
+    {
+        private HubConnection? _connection;
+
+        public async Task StartAsync()
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/chat")
+                .WithAutomaticReconnect()
+                .Build();
+
+            _connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{user}: {message}");
+            });
+
+            await _connection.StartAsync();
+            Console.WriteLine("Connecting to the server...");
+        }
+
+        public async Task SendMessageAsync(string user, string message)
+        {
+            if (_connection == null)
+            {
+                return;
+            }
+
+            if (_connection.State == HubConnectionState.Connected)
+            {
+                await _connection.InvokeAsync("SendMessage", user, message);
+            }
+        }
+    }
+}
